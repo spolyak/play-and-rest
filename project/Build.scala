1b31b3dbb320bd57e047bcf943b12a5ec22be33f
Steve Polyak <stevepolyak@mac.com>
2013-12-02 16:39:49 -0600

    update

--- project/Build.scala
+++ project/Build.scala
@@ -8,8 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
-      "net.databinder.dispatch" %% "dispatch-core" % "0.11.0"
+      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
